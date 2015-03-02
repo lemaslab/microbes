293f0b9478a33602fb0365aca0a6c870224fa296
Dominick J. Lemas <dominicklemas@gmail.com>
2015-03-02 05:30:03 -0700

    update 02Mar15

--- scripts/twin_demo_script.R
+++ scripts/twin_demo_script.R
@@ -15,7 +15,7 @@ install_github("dlemas/microbes")
 library(microbes)
 
 # Twin Study Data
-meta <- twin_meta
+meta <- twin.meta.18mz
 otu.phylum <- twin.18mz.phylum
 otu.family <- twin.18mz.family
 otu.genus <- twin.18mz.genus
@@ -25,18 +25,15 @@ wgs.counts <- twin.wgs.18mz.ko.l4
 ################################        Checking data format         ################################
 ##################################################################################################### 
 
+otu.counts=otu.family
+
 error <- meta_ID_error(meta, otu.counts); dim(error)
 matches <- meta_ID_match(meta, otu.counts); dim(matches)
 
 # Sort metadata by BMI
-meta.bmi <- meta_group_sort(meta, meta$bmi_group)
+names(meta)
+meta.bmi <- meta_group_sort(meta, meta$sample.ObesityCat)
 
-#####################################################################################################
-##########################  metadata analyisis (including hm variables)    ##########################
-#####################################################################################################
-
-#meta.bmi.compare <- otu_abundance_compare(meta[,c(1,3,4,5)], meta,"bmi_group","lean","obese")
-##!!!! This function does not work because we have no numeric entries in the metadata file !!!!
 
 #####################################################################################################
 ##########################          otu data prep and analysis             ##########################
@@ -48,38 +45,34 @@ sum(otu.normed[,2])
 ## Drop OTUs that occur in < 4 participants
 otu.normed <- prevalence_crop(otu.normed, 0.2) ## removed 5
 
-##Crop out bugs with low relative abundance bugs 
+## Crop out bugs with low relative abundance bugs 
 otu.normed <- abundance_crop(otu.normed, 0.001) ## removed 3
 
-## Relative Abundance Comparision   
-# New group compare function
-all.bmi.compare <- group_compare(otu.normed, meta, meta$study_id, meta$bmi_group)
+## Calculate Diversity   
+div.table=alpha_diversity_calc(otu.counts, meta$sample_name, meta$sample.twin_mother)
 
-# old group compare function
-all.bmi.compare2 <- otu_abundance_compare(otu.normed, meta, "bmi_group", "lean", "Obese")
-
-
-## alpha_diversity_calc.R   
-div.table=alpha_diversity_calc(otu.counts, meta$study_id, meta$bmi_group)
-div.mean.sd = otu_abundance_compare(div.table, meta, "bmi_group", "NW", "Ob")
+## Compare Accordint to twin grouping   
+twin.compare <- group_compare(otu.normed, meta, meta$sample_name, meta$sample.twin_mother)
+div.compare = group_compare(div.table, meta, meta$sample_name, meta$sample.twin_mother)
 
 # **************************************************************************** #
 # ***************                   category2pathway.R                         #
 # **************************************************************************** #
 
-# Reduce COG genes to COG pathways
-df.wgs.pathway.counts=category2pathway(wgs.counts,category2pathway.cog)
+# Reduce KO terms to KO pathways
+wgs.counts.l3=gene2path(wgs.counts,ko.map, 3)
 
 # **************************************************************************** #
 # ***************             poison_regression_permute.R                      #
 # **************************************************************************** #
 
-# Input Parameters (need to change the groupings to be consistent with paper!)
-wgs.pathways=df.wgs.pathway.counts
-n.total=11
-n.gr1=6
-n.gr2=5
-twin.wgs.pathway.fit=poison_regression_permute(wgs.pathways,n.total,n.gr1,n.gr2)
+wgs_path_count=wgs.counts.l3
+n.total=length(names(wgs_path_count))
+n.gr1=as.numeric(table(meta.bmi$bmi_group)["NW"])
+n.gr2=as.numeric(table(meta.bmi$bmi_group)["OW/Ob"])
+
+# run model (need a bigger computer!)
+# wgs.path.L3.fit=poison_regression_permute(wgs_path_count,n.total,n.gr1,n.gr2)
 
 # **************************************************************************** #
 # ***************                   dissim_compare.R                    #
@@ -87,7 +80,7 @@ twin.wgs.pathway.fit=poison_regression_permute(wgs.pathways,n.total,n.gr1,n.gr2)
 
 # Measure the Morisita Horn distance between participants
 
-d.index <- dissim_compare(otu.normed, meta, "bmi_group", "lean", "obese")
+d.index <- dissim_compare(otu.normed, meta, "sample.twin_mother", "Twin", "Mom")
 dissim_graph(d.index)
 
 meta.fake <- meta ## I am making a fake data frame to compare twins by pairs
